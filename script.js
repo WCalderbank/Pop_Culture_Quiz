f2fbb0711b53850ef49c34db1d3c413cfca96512
WCalderbank <wcalderbank99@gmail.com>
2022-02-14 14:24:18 +0000

    Part1

---
+++ script.js
@@ -0,0 +1,113 @@
+const quizInfo = [
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+  {
+    question:
+      "What year did Marty Mcfly get stuck in during'Back to the Future'",
+    a: "1965",
+    b: "1955",
+    c: "1975",
+    d: "1945",
+    answer: "B",
+  },
+  {
+    question:
+      "In the tv show Scrubs, the main character JD is consistantly tormented and bullied by whom?",
+    a: "A Garbage-man",
+    b: "A Male Nurse",
+    c: "A Janitor",
+    d: "A Cafeteria Cook",
+    answer: "C",
+  },
+  {
+    question: "The Term Pokemon is short for what phrase?",
+    a: "Poking Monsters",
+    b: "Portable Monsters",
+    c: "Pintsize Monsters",
+    d: "Pocket Monsters",
+    answer: "D",
+  },
+  {
+    question:
+      "What is the name of mercinary Jayne Cobb's gun in the show Firefly",
+    a: "Vera",
+    b: "Vicky",
+    c: "Veronica ",
+    d: "Victoria",
+    answer: "A",
+  },
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+  {
+    question:
+      "In Game of Thrones the motto - Winter is Coming - are the words of which House?",
+    a: "House Targaren",
+    b: "House Karstark",
+    c: "House Stark",
+    d: "House Lannister",
+    answer: "C",
+  },
+];
+const questionData = document.getElementById("question-data");
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
+const a4 = document.getElementById("a4");
+
+let singleQuestion = 0;
+
+newQuestion();
+
+function newQuestion() {
+  singleQuestion++;
+}
